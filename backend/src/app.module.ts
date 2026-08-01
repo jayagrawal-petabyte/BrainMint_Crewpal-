e6a3ba28a412ad0746172de7ec5811029de928b3
Devansh Garg <degarg3245@gmail.com>
2026-08-01 23:28:45 +0530

    feat: implement comments module with RBAC and IDOR protection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { ProjectsModule } from './projects/projects.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -19,8 +20,9 @@ import { DashboardModule } from './dashboard/dashboard.module';
     OrganizationsModule,
     ProjectsModule,
     DashboardModule,
+    CommentsModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
