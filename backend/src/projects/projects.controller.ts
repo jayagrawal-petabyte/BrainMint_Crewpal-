897a6a989fd60201795cdff5e6be342fb6250bf1
pastrystrawberry <pastrystrawberry99@gmail.com>
2026-08-06 21:27:14 +0530

    feat(projects): update projects controller

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import {
   Body,
   Controller,
@@ -12,63 +11,44 @@ import {
   UseGuards,
 } from '@nestjs/common';
 
-import { Role } from '../common/constants/roles.constant';
-import { Roles } from '../common/decorators/roles.decorator';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
-import { RolesGuard } from '../common/guards/roles.guard';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { ProjectsService } from './projects.service';
 
 @Controller('projects')
-@UseGuards(JwtAuthGuard, RolesGuard)
+@UseGuards(JwtAuthGuard)
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
-  @Roles(
-    Role.SUPER_ADMIN,
-    Role.ORG_ADMIN,
-    Role.PROJECT_ADMIN,
-    Role.PROJECT_MANAGER,
-  )
-  create(@Body() dto: CreateProjectDto, @Req() req: any) {
+  create(
+    @Body() dto: CreateProjectDto,
+    @Req() req: any,
+  ) {
     return this.projectsService.create(dto, req.user);
   }
 
   @Get()
-  findAll(@Req() req: any) {
-    return this.projectsService.findAll(req.user);
+  findAll() {
+    return this.projectsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number, @Req() req: any) {
-    return this.projectsService.findOne(id, req.user);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.projectsService.findOne(id);
   }
 
   @Patch(':id')
-  @Roles(
-    Role.SUPER_ADMIN,
-    Role.ORG_ADMIN,
-    Role.PROJECT_ADMIN,
-    Role.PROJECT_MANAGER,
-  )
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() dto: UpdateProjectDto,
-    @Req() req: any,
   ) {
-    return this.projectsService.update(id, dto, req.user);
+    return this.projectsService.update(id, dto);
   }
 
   @Delete(':id')
-  @Roles(
-    Role.SUPER_ADMIN,
-    Role.ORG_ADMIN,
-    Role.PROJECT_ADMIN,
-    Role.PROJECT_MANAGER,
-  )
-  remove(@Param('id', ParseIntPipe) id: number, @Req() req: any) {
-    return this.projectsService.deactivate(id, req.user);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.projectsService.deactivate(id);
   }
 }
