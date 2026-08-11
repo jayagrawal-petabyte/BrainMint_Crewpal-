844fe8cc3c308f71ad4aacd026d7db39200cc9d3
Farjan Alam <115532270+SyntaxSaviour@users.noreply.github.com>
2026-08-11 22:38:08 +0530

    fix(app): Import ReportsModule in app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-
+import { ReportsModule } from './reports/reports.module';
 import { DatabaseModule } from './database/db.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
